1e53d94720f8a7576c7576aea4d6bc76472d0b4b
Alfredo Rafael Fernandez Zamora <alfredorafael@gmail.com>
2016-10-11 15:19:57 -0500

    Pruebas con Twig

---
+++ src/AppBundle/Controller/InicioController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raffaello
+ * Date: 11/10/16
+ * Time: 14:20
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class InicioController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        $plantillas = $this->container->get('templating');
+
+        $html = $plantillas->render("base.html.twig");
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/{mensaje}")
+     */
+    public function dynamicAction($mensaje)
+    {
+        $plantillas = $this->container->get('templating');
+
+        $html = $plantillas->render("prueba/prueba.html.twig", ["mensaje" => $mensaje]);
+        return new Response($html);
+    }
+}
